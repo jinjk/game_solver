4cf6b602404610f159d906d9d55726f3950788f1
jjin <jiankun.jin@gmail.com>
2017-02-07 23:44:42 +0800

    add python Dockerfile with ubuntu

---
+++ python_docker/download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget -O python.tar.xz https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tar.xz \
+&& wget -O python.tar.xz.asc https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tar.xz.asc \
+\
